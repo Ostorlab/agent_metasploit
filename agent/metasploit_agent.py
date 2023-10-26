8af86ae5f2552acf8940afd299d6960970de38bd
ostorlab <support@ostorlab.co>
2023-10-26 12:11:08 +0100

    - Fix typing

--- agent/metasploit_agent.py
+++ agent/metasploit_agent.py
@@ -134,7 +134,7 @@ class MetasploitAgent(
             risk_rating=vuln_mixin.RiskRating.HIGH,
         )
 
-    def _set_module_args(self, selected_module, vhost, rport) -> msfrpc.MsfModule:
+    def _set_module_args(self, selected_module: msfrpc.MsfModule, vhost: str, rport: int) -> msfrpc.MsfModule:
         rhost = socket.gethostbyname(vhost)
         if "RHOSTS" not in selected_module.required:
             raise ArgumentError(
