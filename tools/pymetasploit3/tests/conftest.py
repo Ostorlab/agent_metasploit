8341abaf0c139ab1900e23b9d890384fe01778e6
ostorlab <support@ostorlab.co>
2023-10-31 10:59:58 +0100

    pymetasploit only

---
+++ tools/pymetasploit3/tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+import subprocess
+from pymetasploit3 import msfrpc
+
+MSFRPCD_PWD = "Ostorlab123"
+
+
+@pytest.fixture(scope="module")
+def client() -> msfrpc.MsfRpcClient:
+    """Start msfrpcd and connect to it"""
+    command = ["msfrpcd", "-P", MSFRPCD_PWD, "-p", "55552"]
+    subprocess.run(command, capture_output=False, check=False)
+    client = msfrpc.MsfRpcClient(MSFRPCD_PWD, ssl=True, port=55552)
+    return client
