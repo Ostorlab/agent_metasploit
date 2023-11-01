a3e5da28e56c1f665ce426137ae9a18856f04df4
ostorlab <support@ostorlab.co>
2023-11-01 11:34:05 +0100

    use port 55552

--- agent/utils.py
+++ agent/utils.py
@@ -14,7 +14,7 @@ def initialize_msf_rpc() -> msfrpc.MsfRpcClient:
         - msfrpc client
     """
     msfrpc_pwd = "".join([random.choice(string.ascii_letters) for _ in range(12)])
-    command = ["msfrpcd", "-P", msfrpc_pwd, "-p", "55557"]
+    command = ["msfrpcd", "-P", msfrpc_pwd, "-p", "55552"]
     subprocess.run(command, capture_output=False, check=False)
-    client = msfrpc.MsfRpcClient(msfrpc_pwd, ssl=True, port=55557)
+    client = msfrpc.MsfRpcClient(msfrpc_pwd, ssl=True, port=55552)
     return client
