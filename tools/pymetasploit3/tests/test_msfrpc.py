d9423d2dc6704f3a3744ac95c60f922a0891e341
benyissa <benyissa@ostorlab.dev>
2024-01-18 18:36:02 +0100

    [skip ci] Use Ruff as a linter and formatter; update all

--- tools/pymetasploit3/tests/test_msfrpc.py
+++ tools/pymetasploit3/tests/test_msfrpc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import pytest
-from pymetasploit3.msfrpc import *
+from pymetasploit3.msfrpc import *  # noqa: F403
 
 
 @pytest.fixture()
@@ -13,12 +13,12 @@ def cid(client):
 
 
 def test_jobs(client):
-    assert type(client.jobs.list) == dict
+    assert type(client.jobs.list) == dict  # noqa: E721
 
 
 def test_login(client):
-    assert isinstance(client, MsfRpcClient)
-    tl = client.call(MsfRpcMethod.AuthTokenList)
+    assert isinstance(client, MsfRpcClient)  # noqa: F405
+    tl = client.call(MsfRpcMethod.AuthTokenList)  # noqa: F405
     assert "tokens" in tl
     assert len(tl["tokens"]) > 1  # There should be temp token and UUID perm token
     nontemp_token = False
@@ -28,9 +28,9 @@ def test_login(client):
         if "TEMP" not in x:
             nontemp_token = True
             break
-    assert nontemp_token == True
+    assert nontemp_token == True  # noqa: E712
 
 
 def test_pluginloaded(client):
-    plugins = client.call(MsfRpcMethod.PluginLoaded)
+    plugins = client.call(MsfRpcMethod.PluginLoaded)  # noqa: F405
     assert "msgrpc" in plugins["plugins"]
